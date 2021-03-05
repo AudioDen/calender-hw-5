ffc851b890b923780e669b96987916e170baaf4c
AudioDen <chicagosoundman@gmail.com>
2021-03-05 17:56:49 -0600

    clock/day--added time blocks

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
+
+
 var timeDisplayEl = $('#time-display');
+var todayEl = $('#currentDay');
+//shows date and time
 function displayTime() {
-    var rightNow = moment().format('MMM DD, YYYY [  ] hh:mm:ss a');
+    var rightNow = moment().format(' MMM DD, YYYY [  ] hh:mm:ss a');
     timeDisplayEl.text(rightNow);
-  }
+  };
+//show the day
+  function dayDisplay(){
+    var todayNow = moment().format('dddd');
+    todayEl.text(todayNow);
+
+  };
+//calling the two function to operate
   setInterval(displayTime, 1000);
+  setInterval(dayDisplay, 1000)
+
+
+  function watchTower(){
+      
+  }
