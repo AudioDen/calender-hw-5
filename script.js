7664fc2f1a90b2a587e22386d0781072d5f21b85
AudioDen <chicagosoundman@gmail.com>
2021-03-06 16:52:02 -0600

    trying to get the background to work right

--- script.js
+++ script.js
@@ -1,32 +1,65 @@
 
+$(document).ready(function () {
 
-var timeDisplayEl = $('#time-display');
-var todayEl = $('#currentDay');
-//shows date and time
-function displayTime() {
-    var rightNow = moment().format(' MMM DD, YYYY [  ] hh:mm:ss a');
-    timeDisplayEl.text(rightNow);
-};
-//show the day
-function dayDisplay() {
-    var todayNow = moment().format('dddd');
-    todayEl.text(todayNow);
 
-};
-//calling the two function to operate
-setInterval(displayTime, 1000);
-setInterval(dayDisplay, 1000);
+    var timeDisplayEl = $('#time-display');
+    var todayEl = $('#currentDay');
+    //shows date and time
+    function displayTime() {
+        var rightNow = moment().format(' MMM DD, YYYY [  ] hh:mm:ss a');
+        timeDisplayEl.text(rightNow);
+    };
+    //show the day
+    function dayDisplay() {
+        var todayNow = moment().format('dddd');
+        todayEl.text(todayNow);
 
-$(".saveBtn").on("click", function () {
-    // Get nearby values of the description in JQuery
-    var text = $(this).siblings(".description").val();///// gets it from the textarea
-    var time = $(this).parent().attr("id");////parent div of the hour
+    };
+    //calling the two function to operate
+    setInterval(displayTime, 1000);
+    setInterval(dayDisplay, 1000);
 
-    // Save text from textarea in local storage
-    localStorage.setItem(time, text);
+    $(".saveBtn").on("click", function () {
+        // Get nearby values of the description in JQuery
+        var text = $(this).siblings(".description").val();///// gets it from the textarea
+        var time = $(this).parent().attr("id");////parent div of the hour
+
+        // Save text from textarea in local storage
+        localStorage.setItem(time, text);
+    })
+    /////you got to pull this back 
+
+
+    //////changing color
+    //if current hour is equal to the time hour then css needs to be current
+    //if current hour is not equal to current hour css should reflect past or present 
+    //
+    function watchTower() {
+        var currentHour = moment().hour; //hour track
+        console.log(currentHour);
+
+        $(".time-block").each(function () {
+            var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+            console.log(blockHour, currentHour)
+
+            if (blockHour < currentHour) {
+                $(this).addClass("past");
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+            }
+            if (blockHour === currentHour) {
+                $(this).addClass("present");
+                $(this).removeClass("future");
+                $(this).removeClass("past");
+            }
+            else {
+                $(this).addClass("future");
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+            }
+        })
+
+
+    }
+    watchTower();
 })
- /////you got to pull this back 
-//////changing color
-function watchTower() {
-
-}
